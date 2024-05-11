659b0798a710c2f4ee96930b09b21c5afc93927d
Jacob <EarthTek19@gmail.com>
2024-05-11 13:52:41 -0500

    dynamic array methods complete

--- dynamic-array.js
+++ dynamic-array.js
@@ -8,15 +8,18 @@ class DynamicArray {
   }
 
   read(index) {
-return this.data[index]
+    return this.data[index]
   }
 
   push(val) {
-// Add val to end of array (this.data)
-// increase the capacity! Increase by one from the defaultSize
-//      if length >= current capacity, then resize the capacity
-// Make the storage array with the specified cap
-// set our data = to the copy data 
+    // Add val to end of array (this.data)
+    // increase the capacity! Increase by one from the defaultSize
+    //      if length >= current capacity, then resize the capacity
+    // Make the storage array with the specified cap
+    // set our data = to the copy data
+    if (this.length >= this.capacity) {
+      this.resize();
+    }
 
     this.data[this.length] = val;
     this.length++;
@@ -25,31 +28,61 @@ return this.data[index]
 
 
   pop() {
-
-
-
-
-    
-  }
-
-  shift() {
-
-    // Your code here 
+    //if array has elements remove last element and return it
+    //if array is empty return undefined
+    if (this.length > 0) {
+      this.length --;
+      return this.data[this.length];
+    } else {
+      return undefined;
+    }
   }
 
   unshift(val) {
-
-    // Your code here 
+    if (this.length >= this.capacity) {
+      this.resize();
+    }
+    for (let i = this.length; i > 0; i--) {
+      this.data[i] = this.data[i - 1]
+    }
+    this.length++;
+    this.data[0] = val;
   }
+
+
+  shift() {
+    if (this.length > 0) {
+      let result = this.data[0];;
+      for (let i = 0; i < this.length; i++) {
+        this.data[i] = this.data[i + 1]
+      }
+      this.length--;
+      return result;
+    } else {
+      return undefined;
+    }
+ }
+
 
   indexOf(val) {
-
-    // Your code here 
+    for (let i = 0; i < this.length; i++) {
+      if (this.data[i] === val) {
+        return i;
+      }
+    }
+    return -1;
   }
 
-  resize() {
 
-    // Your code here 
+  resize() {
+    if (this.length >= this.capacity) {
+      this.capacity *= 2;
+      let result =  Array(this.capacity);
+      for (let i = 0; i < this.length; i++) {
+        result[i] = this.data[i]
+      }
+      this.data = result;
+    }
   }
 
 }
