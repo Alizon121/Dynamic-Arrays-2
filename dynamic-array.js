1d25b8d2cf9c63cc605ccaaa00637c37c5419716
Andrew <andrew.lizon12@gmail.com>
2024-05-11 10:04:47 -0700

    3/10 tests passing

--- dynamic-array.js
+++ dynamic-array.js
@@ -1,24 +1,53 @@
 class DynamicArray {
 
   constructor(defaultSize = 4) {
+    this.capacity = defaultSize
+    this.length = 0
+    this.data = new Array(defaultSize)
 
-    // Your code here 
   }
 
   read(index) {
-
-    // Your code here 
+return this.data[index]
   }
 
   push(val) {
+// Add val to end of array (this.data)
 
-    // Your code here 
+// Returns 0 at line 57
+// this.data[this.data.length] = val
+// return this.data.length
+
+
+
+for (let i = 0; i < this.data.length; i++) {
+  this.data[i] = this.data[i+1]
+  // console.log(this.data[i])
+}
+// this.length++
+// this.data[this.data.length -1] = val
+
+
+
+
+// Returns 2 at line 62
+// if (this.data.length === 1) {
+// this.length++
+// this.data[0] = val
+// } 
+// else {
+//   this.data[this.data.length - 1] = val
+//   this.length++
+// }
   }
 
 
   pop() {
 
-    // Your code here 
+
+
+
+    
   }
 
   shift() {
